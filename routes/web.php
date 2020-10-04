ede2b089c69d49062f749f55770b0424fa971986
SAADRMILI <saadrmili45@gmail.com>
2020-10-04 21:35:40 +0100

    API Usage & HTTP Client - Part 2

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/show', function () {
-    return view('show');
-});
+Route::get('/','MoviesController@index')->name('movies.index');
+Route::get('/movies/{movie}','MoviesController@show')->name('movies.show');
+// Route::get('/','MoviesController@index')->name('movies.index');
+
